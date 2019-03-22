470af6f5ac4ac86d41ea06f0f8a102b7c2bec497
Mohammed Gomaa <promgom3a@gmail.com>
2019-03-22 17:07:43 +0200

    list group friends

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  resources :groups,  except: [ :user_groups ] do 
+    get '/users', to: 'users#group_users'
+  end 
+  resources :users do 
+    get '/groups', to: 'users#user_groups'
+  end 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
